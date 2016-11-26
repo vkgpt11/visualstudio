11c2f2c1d3d4f3c7bb70fc8637d6df64bc355290
vkgpt11 <vikasgupta.net@live.com>
2016-11-26 07:56:19 +0530

    Initial commit

---
+++ Projects/PhotoStudioInAngular/PhotoStudioInAngular/_reference.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true"/>
+/// <reference path="Angular/lib/angular.js"/>
+/// <reference path="app.js"/>
+/// <reference path="Angular/lib/angular.min.js" />
+/// <reference path="Angular/lib/angular.route.min.js" />
---
+++ Projects/PhotoStudioInAngular/PhotoStudioInAngular/app.js
@@ -0,0 +1,21 @@
+﻿/// <autosync enabled="true"/>
+/// <reference path="Angular/lib/angular.js"/>
+/// <reference path="app.js"/>
+/// <reference path="Angular/lib/angular.min.js" />
+/// <reference path="Angular/lib/angular.route.min.js" />
+
+
+angular.module("photoBrowser", []).service("imageService", function ($http) {
+    return {
+        getImages: function () {
+            return $http.get("/JsonData/Imagesjson.json", { responseType: "json" });
+        }
+    }
+})
+.controller("photoList", function ($scope,imageService) {
+    $scope.images = imageService.getImages().then(function (images) {
+        $scope.images = images.data;
+        console.log($scope.images);
+    });
+    //
+})
